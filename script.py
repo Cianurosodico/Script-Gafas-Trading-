d8f3a2c0f38fb6b2df3b594b01819efbc2061b4d
cianurosodico <jccanchilas@unal.edu.co>
2023-07-05 20:46:14 -0500

    no api needed

--- script.py
+++ script.py
@@ -1,9 +1,19 @@
 from functions import *
 
-ticks = buscarTicks()
-ticksNumber = len(ticks)
-print("NUMERO DE TICKS: " + str(ticksNumber))
 
-for tick in ticks:
-    analizarMoneda(tick)
-showResults()
+days = 7
+
+
+
+if __name__ == "__main__":
+
+    try :
+        days = int(eval(input("Analizar x días atrás : ")))
+    except :
+        pass
+    ticks = buscarTicks()
+    ticksNumber = len(ticks)
+    print("NUMERO DE TICKS: " + str(ticksNumber))
+    for tick in ticks:
+        analizarMoneda(tick,days)
+    showResults()
